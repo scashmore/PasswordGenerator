1ba5c188c684dae13396c7095bc16a923ac6c90a
Susan Ashmore <scashmorecitcom@gmail.com>
2021-02-14 22:54:42 -0500

    js vars

---
+++ assets/js/script.js
@@ -0,0 +1,15 @@
+//GENERATOR
+
+//var set ups
+var lowercase = "abcdefghijklmnopqrstuvwxyz";
+var uppercase = lowercase.toUpperCase();
+var numbers = "0123456789";
+var symbols = "!@#$%^&*";
+var lower = lowercase.split("");
+var upper = uppercase.split("");
+var nums = numbers.split("");
+var syms = symbols.split("");
+console.log(lower);
+console.log(upper);
+console.log(nums);
+console.log(syms);
