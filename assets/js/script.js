32b1253b8dcd8c31f0489b1f52a798aa48c61235
Susan Ashmore <scashmorecitcom@gmail.com>
2021-02-14 23:34:16 -0500

    updated random generator functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -9,36 +9,36 @@ var lower = lowercase.split("");
 var upper = uppercase.split("");
 var nums = numbers.split("");
 var syms = symbols.split("");
-console.log(lower);
-console.log(upper);
-console.log(nums);
-console.log(syms);
+//console.log(lower);
+//console.log(upper);
+//console.log(nums);
+//console.log(syms);
 
 //random gen functions
 function randLower() {
-    var i = Math.floor(Math.random() * lower.length - 1);
-    return lower[i];
+    return lower[Math.floor(Math.random() * lower.length - 1];
 }
 function randUpper() {
-    var i = Math.floor(Math.random() * upper.length - 1);
-    return upper[i];
+    return upper[Math.floor(Math.random() * upper.length - 1)];
 };
 
-function randnum() {
-    var i = Math.floor(Math.random() * nums.length - 1);
-    return nums[i];
+function randNum() {
+    return nums[Math.floor(Math.random() * nums.length - 1)];
 };
 
-function randsym() {
-    var i = Math.floor(Math.random() * sym.length - 1);
-    return syms[i];
+function randSym() {
+    return syms[Math.floor(Math.random() * sym.length - 1)];
 };
+//console.log(randUpper());
+//console.log(randLower());
+//console.log(randNum());
+//console.log(randSym());
 
-//SLIDER
-var slide = document.getElementById("slide");
-var output = document.getElementById("charValue");
-output.innerHTML = slide.value;
+//SLIDER **COME BACK NOT FUCTIONING!**
+//var slide = document.getElementById("slide");
+//var output = document.getElementById("charValue");
+//output.innerHTML = slide.value;
 
-slide.input = function() {
-    output.innerHTML = this.value;
-};
+//slide.input = function() {
+//    output.innerHTML = this.value;
+//};
