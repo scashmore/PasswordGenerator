c535e1b2e0478fa59a96968220bced180b6a383f
Susan Ashmore <scashmorecitcom@gmail.com>
2021-02-14 23:25:34 -0500

    js gens and slider

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,32 @@ console.log(lower);
 console.log(upper);
 console.log(nums);
 console.log(syms);
+
+//random gen functions
+function randLower() {
+    var i = Math.floor(Math.random() * lower.length - 1);
+    return lower[i];
+}
+function randUpper() {
+    var i = Math.floor(Math.random() * upper.length - 1);
+    return upper[i];
+};
+
+function randnum() {
+    var i = Math.floor(Math.random() * nums.length - 1);
+    return nums[i];
+};
+
+function randsym() {
+    var i = Math.floor(Math.random() * sym.length - 1);
+    return syms[i];
+};
+
+//SLIDER
+var slide = document.getElementById("slide");
+var output = document.getElementById("charValue");
+output.innerHTML = slide.value;
+
+slide.input = function() {
+    output.innerHTML = this.value;
+};
